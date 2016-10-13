86309bbaff52d56652c3ac8de24f13034691d22f
Ryan Dumlao <dlogsr@gmail.com>
2016-10-13 10:56:33 -0700

    remove URL bar

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 $whiteArtifact = $('.white-artifact');
 
+window.addEventListener("load", function() { window. scrollTo(0, 0); });
+document.addEventListener("touchmove", function(e) { e.preventDefault() });
+
+
 $whiteArtifact.pep();
