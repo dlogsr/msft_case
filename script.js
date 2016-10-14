2cacecc164c08a80f605e331c91f6834b5c9c761
Ryan Dumlao <dlogsr@gmail.com>
2016-10-13 23:18:13 -0700

    more fullscreen tries

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ $whiteArtifact = $('.white-artifact');
 window.addEventListener("load", function() { window. scrollTo(0, 0); });
 document.addEventListener("touchmove", function(e) { e.preventDefault() });
 
+window.scrollTo(0,1);
 
 $whiteArtifact.pep();
