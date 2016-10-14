0cf2eb949b21571d973f15a02984f44aa051ad67
Ryan Dumlao <dlogsr@gmail.com>
2016-10-14 01:56:21 -0700

    created multiple pages. top-right toggle to reset / 'pull in' a slyde'

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $whiteArtifact = $('.white-artifact');
+$hButton = $('.hidden-toggle');
 
 window.addEventListener("load", function() { window. scrollTo(0, 0); });
 document.addEventListener("touchmove", function(e) { e.preventDefault() });
@@ -6,3 +7,16 @@ document.addEventListener("touchmove", function(e) { e.preventDefault() });
 window.scrollTo(0,1);
 
 $whiteArtifact.pep();
+
+$(document).ready(function(){
+	$hButton.on('tap',function(){
+		console.log('test');
+		// $whiteArtifact.data('plugin_pep').revert();
+		$whiteArtifact.removeAttr('style');
+		$whiteArtifact.animate({top:'-300'},0)
+		setTimeout(function(){
+			
+			$whiteArtifact.animate({top:'+=600'},1000,"easeOutExpo");
+		},500);
+	});
+});
